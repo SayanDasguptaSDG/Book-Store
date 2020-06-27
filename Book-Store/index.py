163a10f02badfdbabc1370312169bacc080302a5
Sayan Dasgupta <sayan.dg1995@gmail.com>
2020-06-27 20:27:51 +0530

    divided the logic into by creating functions. also implemented the search feature.

--- Book-Store/index.py
+++ Book-Store/index.py
@@ -1,72 +1,122 @@
 # repository to store books
 books = []
 
-choice = input("""A for adding a book,
-    R for reading a book,
-    D for deleting a book,
-    U for updating a book,
-    Q for quitting the menu.
-    Enter your choice : """).upper()
 
+# user menu
+def userMenu():
+    menu = input("""A for adding a book,
+R for reading a book,
+D for deleting a book,
+U for updating a book,
+S for searching a book,
+Q for quitting the menu.
+Enter your choice : """).upper()
+    return menu
+
+
+# update menu
+def updateByMenu():
+    ch = input("""What do you want to update? 
+T for title, 
+A for author, 
+Y for year, 
+N for nothing : """).upper()
+    return ch
+
+
+# add a book
+def addBook():
+    title = input("Enter book title : ")
+    author = input("Enter author name : ")
+    year = input("Enter publishing year : ")
+    books.append({
+        'title': title,
+        'author': author,
+        'year': year
+    })
+
+
+# delete a book
+def deleteBook():
+    if len(books) == 0:
+        print("No books are there for deletion. Please try after adding some books.")
+    else:
+        delbook = input("Please mention the book name : ")
+        for i in range(len(books)):
+            if books[i]['title'] == delbook:
+                del books[i]
+                break
+            else:
+                print("Mentioned book is not present")
+
+
+# read a book
+def readBook():
+    if len(books) == 0:
+        print("No books are yet added. Please try again.")
+    else:
+        for i in range(len(books)):
+            print(books[i])
+
+
+# update a book
+def updateBook():
+    if len(books) == 0:
+        print("No books are there for updation. Please try after adding some books.")
+    else:
+        updatebook = input("Enter book to be updated : ")
+        for i in range(len(books)):
+            if books[i]['title'] == updatebook:
+                ch = updateByMenu()
+                while ch != "N":
+                    if ch == "T":
+                        books[i]['title'] = input("Enter the desired title : ")
+                    elif ch == 'A':
+                        books[i]['author'] = input("Enter the desired author : ")
+                    elif ch == 'Y':
+                        books[i]['year'] = input("Enter the desired year : ")
+                    else:
+                        print("Wrong choice")
+                    ch = updateByMenu()
+            else:
+                print("Mentioned book is not present")
+
+
+#search a book
+def searchBook():
+    if len(books) == 0:
+        print("No books are there to be searched. Please try after adding some books.")
+    else:
+        searchbook = input("Search by title : ")
+        for i in range(len(books)):
+            if searchbook == books[i]['title']:
+                print(books[i])
+            else:
+                print("Mentioned book is not present")
+
+
+#accept user choice
+choice = userMenu()
+
+#exit condition
 while choice != "Q":
 
     if choice == "A":
-        title = input("Enter book title : ")
-        author = input("Enter author name : ")
-        year = input("Enter publishing year : ")
-        books.append({
-            'title': title,
-            'author': author,
-            'year': year
-        })
+        addBook()
 
     elif choice == "R":
-        if len(books) == 0:
-            print("No books are yet added. Please try again.")
-        else:
-            for i in range(len(books)):
-                print(books[i])
+        readBook()
 
     elif choice == "D":
-        if len(books) == 0:
-            print("No books are there for deletion. Please try after adding some books.")
-        else:
-            delBook = input("Please mention the book name : ")
-            for i in range(len(books)):
-                if books[i]['title'] == delBook:
-                    del books[i]
-                    break
-            print(books)
-
+        deleteBook()
 
     elif choice == "U":
-        if len(books) == 0:
-            print("No books are there for deletion. Please try after adding some books.")
-        else:
-            updateBook = input("Enter book to be updated : ")
-            for i in range(len(books)):
-                if books[i]['title'] == updateBook:
-                    ch = input("What do you want to update? T for title, A for author, Y for year, N for nothing : ").upper()
-                    while ch != "N":
-                        if ch == "T":
-                            books[i]['title'] = input("Enter the desired title : ")
-                        elif ch == 'A':
-                            books[i]['author'] = input("Enter the desired author : ")
-                        elif ch == 'Y':
-                            books[i]['year'] = input("Enter the desired year : ")
-                        else:
-                            print("Wrong choice")
-                        ch = input(
-                            "What do you want to update? T for title, A for author, Y for year, N for nothing : ").upper()
-                else:
-                    pass
+        updateBook()
+
+    elif choice == "S":
+        searchBook()
 
     else:
         print("Something wrong")
 
-    choice = input("""\nA for adding a book,
-    R for reading a book,
-    D for deleting a book,
-    U for updating a book,
-    Q for quitting the menu.
-    Enter your choice : """).upper()
+    choice = userMenu()
