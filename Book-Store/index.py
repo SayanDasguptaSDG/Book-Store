1c4157e0c5831aed2d554c54f2c2618ab008f359
Sayan Dasgupta <sayan.dg1995@gmail.com>
2020-06-28 18:35:23 +0530

    implemented searby features based on title, author and year.
    also improved the searching and looping mechanism

--- Book-Store/index.py
+++ Book-Store/index.py
@@ -38,16 +38,20 @@ def addBook():
 
 # delete a book
 def deleteBook():
+    flag = 0
     if len(books) == 0:
         print("No books are there for deletion. Please try after adding some books.")
     else:
-        delbook = input("Please mention the book name : ")
-        for i in range(len(books)):
-            if books[i]['title'] == delbook:
-                del books[i]
+        deletebook = input("Please mention the book name : ")
+        for i in books:
+            if i['title'] == deletebook:
+                books.remove(i)
+                flag = 1
                 break
             else:
-                print("Mentioned book is not present")
+                pass
+        if flag != 1:
+            print("Mentioned book is not present")
 
 
 # read a book
@@ -55,50 +59,116 @@ def readBook():
     if len(books) == 0:
         print("No books are yet added. Please try again.")
     else:
-        for i in range(len(books)):
-            print(books[i])
+        for i in books:
+            print(f"{i['title']} by {i['author']}, {i['year']}")
 
 
 # update a book
 def updateBook():
+    flag = 0
     if len(books) == 0:
-        print("No books are there for updation. Please try after adding some books.")
+        print("No books are there for updating. Please try after adding some books.")
     else:
         updatebook = input("Enter book to be updated : ")
-        for i in range(len(books)):
-            if books[i]['title'] == updatebook:
+        for i in books:
+            if i['title'] == updatebook:
                 ch = updateByMenu()
                 while ch != "N":
                     if ch == "T":
-                        books[i]['title'] = input("Enter the desired title : ")
+                        i['title'] = input("Enter the desired title : ")
                     elif ch == 'A':
-                        books[i]['author'] = input("Enter the desired author : ")
+                        i['author'] = input("Enter the desired author : ")
                     elif ch == 'Y':
-                        books[i]['year'] = input("Enter the desired year : ")
+                        i['year'] = input("Enter the desired publishing year : ")
                     else:
-                        print("Wrong choice")
+                        print("Please enter the choice from the set of choices")
                     ch = updateByMenu()
+                flag = 1
+                break
             else:
-                print("Mentioned book is not present")
+                pass
+        if flag != 1:
+            print("Mentioned book is not present")
+
+def searchMenu():
+    ch = input("""How would you like to search?
+T for title
+A for author
+Y for publishing year
+N for nothing.
+Enter your choice : """).upper()
+    return ch
 
 
-#search a book
+# search a book
 def searchBook():
+    flag = 0
     if len(books) == 0:
         print("No books are there to be searched. Please try after adding some books.")
     else:
-        searchbook = input("Search by title : ")
-        for i in range(len(books)):
-            if searchbook == books[i]['title']:
-                print(books[i])
+        ch = searchMenu()
+        while ch != "N":
+            if ch == "T":
+                flag = searchByTitle(books, flag)
+                break
+            elif ch == "A":
+                flag = searchByAuthor(books, flag)
+                break
+            elif ch == "Y":
+                flag = searchByYear(books, flag)
+                break
             else:
-                print("Mentioned book is not present")
+                print("Wrong choice")
+            ch = searchMenu()
+
+        if ch == "N":
+            flag = 1
+
+        if flag != 1:
+            print("Mentioned book is not present")
 
 
-#accept user choice
+# search by title
+def searchByTitle(books, flag):
+    searchbook = input("Search by title : ")
+    for i in books:
+        if searchbook == i['title']:
+            print(f"{i['title']} by {i['author']}, {i['year']}")
+            flag = 1
+        else:
+            pass
+    return flag
+
+
+# search by author
+def searchByAuthor(books, flag):
+    searchbook = input("Search by author : ")
+    for i in books:
+        if searchbook == i['author']:
+            print(f"{i['title']} by {i['author']}, {i['year']}")
+            flag = 1
+        else:
+            pass
+    return flag
+
+
+# search by publishing year
+def searchByYear(books, flag):
+    searchbook = input("Search by publishing year : ")
+    for i in books:
+        if searchbook == i['year']:
+            print(f"{i['title']} by {i['author']}, {i['year']}")
+            flag = 1
+        else:
+            pass
+    return flag
+
+
+# accept user choice
 choice = userMenu()
 
-#exit condition
+
+# exit condition
 while choice != "Q":
 
     if choice == "A":
