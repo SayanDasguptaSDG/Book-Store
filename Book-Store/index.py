56decdd090e78749d7574f31fc2f2200fd3e58c4
Sayan Dasgupta <sayan.dg1995@gmail.com>
2020-06-29 19:29:36 +0530

    implemented the logic using SQLite db
    moderated the feature handling with functions and SQLite db

--- Book-Store/index.py
+++ Book-Store/index.py
@@ -1,6 +1,45 @@
 from utils import bookDatabase
 
 
+######################################## MENUS ########################################
+
+
+# user menu
+def userMenu():
+    menu = input("""A for adding a book,
+R for reading a book,
+D for deleting a book,
+U for updating a book,
+S for searching a book,
+Q for quitting the menu.
+Enter your choice : """).upper()
+    return menu
+
+
+# update menu
+def updateByMenu():
+    ch = input("""What do you want to update? 
+T for title, 
+A for author, 
+Y for year, 
+N for nothing : """).upper()
+    return ch
+
+
+# search menu
+def searchMenu():
+    ch = input("""How would you like to search?
+T for title
+A for author
+Y for publishing year
+N for nothing.
+Enter your choice : """).upper()
+    return ch
+
+
+######################################## MAIN MENU ########################################
+
+
 def menu():
     bookDatabase.createBook()
     # accept user choice
@@ -22,26 +61,19 @@ def menu():
         choice = userMenu()
 
 
-# user menu
-def userMenu():
-    menu = input("""A for adding a book,
-R for reading a book,
-D for deleting a book,
-U for updating a book,
-S for searching a book,
-Q for quitting the menu.
-Enter your choice : """).upper()
-    return menu
+######################################## FEATURES ########################################
 
 
+# display all books
 def listAllBooks():
-    if len(bookDatabase.get_all_books()) == 0:
+    if bookDatabase.get_all_books() == 0:
         print("Please add some books first.")
     else:
         for book in bookDatabase.get_all_books():
             print(f"{book['title']} by {book['author']} - {book['year']}")
 
 
+# prompt for adding a book into the database
 def addBookPrompt():
     title = input("Enter book title : ")
     author = input("Enter author name : ")
@@ -49,85 +81,57 @@ def addBookPrompt():
     bookDatabase.addBook(title, author, year)
 
 
-def searchMenu():
-    ch = input("""How would you like to search?
-T for title
-A for author
-Y for publishing year
-N for nothing.
-Enter your choice : """).upper()
-    return ch
-
-
-def searchPrompt():
-    if len(bookDatabase.get_all_books()) == 0:
-        print("Please add some books first.")
-    else:
-        ch = searchMenu()
-        while ch != "N":
-            if ch == "T":
-                book = input("Search by title : ")
-                books = bookDatabase.searchByTitle(book)
-                if len(books) == 0:
-                    print("Mentioned book is not present")
-                else:
-                    for i in books:
-                        print(f"{i['title']} by {i['author']} - {i['year']}")
-                    break
-            elif ch == "A":
-                book = input("Search by author : ")
-                books = bookDatabase.searchByAuthor(book)
-                if len(books) == 0:
-                    print("Mentioned book is not present")
-                else:
-                    for i in books:
-                        print(f"{i['title']} by {i['author']} - {i['year']}")
-                break
-            elif ch == "Y":
-                book = input("Search by year : ")
-                books = bookDatabase.searchByYear(book)
-                if len(books) == 0:
-                    print("Mentioned book is not present")
-                else:
-                    for i in books:
-                        print(f"{i['title']} by {i['author']} - {i['year']}")
-                break
-            elif ch == "N":
-                pass
-            else:
-                print("Wrong choice")
-            ch = searchMenu()
-
-
+# prompt for deleting a book from the database
 def deleteBookPrompt():
     book = input("Which book do you want to delete? : ")
     bookDatabase.deleteBook(book)
 
 
-def updateByMenu():
-    ch = input("""What do you want to update? 
-T for title, 
-A for author, 
-Y for year, 
-N for nothing : """).upper()
-    return ch
-
-
-def updateBookPrompt():
-    flag = 0
-    searchedBook = input("Which book needs to be updated? : ")
-    books = bookDatabase.get_all_books()
-    for book in books:
-        if searchedBook == book['title']:
-            flag = 1
-            ch = updateByMenu()
-            bookDatabase.updateBook(searchedBook, ch)
-        else:
+# prompt for searching the book in the database
+def searchPrompt():
+    if len(bookDatabase.get_all_books()) == 0:
+        print("Please add some books first.")
+    else:
+        ch = searchMenu()
+        if ch == "T":
+            searchedTitle = input("Search by title : ")
+            books = bookDatabase.searchBookByTitle(searchedTitle)
+            if len(books) == 0:
+                print("Mentioned book is not present")
+            else:
+                for i in books:
+                    print(f"{i['title']} by {i['author']} - {i['year']}")
+        elif ch == "A":
+            searchedAuthor = input("Search by author : ")
+            books = bookDatabase.searchBookByAuthor(searchedAuthor)
+            if len(books) == 0:
+                print("Books from mentioned author is not present")
+            else:
+                for i in books:
+                    print(f"{i['title']} by {i['author']} - {i['year']}")
+        elif ch == "Y":
+            searchedYear = input("Search by publishing year : ")
+            books = bookDatabase.searchBookByYear(searchedYear)
+            if len(books) == 0:
+                print("Books published in mentioned year is not present")
+            else:
+                for i in books:
+                    print(f"{i['title']} by {i['author']} - {i['year']}")
+        elif ch == "N":
             pass
-    if flag != 1:
-        print("Book is not found")
+        else:
+            print("Wrong choice")
 
 
+# prompt for updating the values into the database
+def updateBookPrompt():
+    searchedBook = input("Which book needs to be updated? : ")
+    if len(bookDatabase.searchBookByTitle(searchedBook)) == 0:
+        print("Mentioned book is not found")
+    else:
+        ch = updateByMenu()
+        bookDatabase.updateBook(searchedBook, ch)
 
 
+# entry point of the application - calling the main menu function
 menu()
--- Book-Store/utils/bookDatabase.py
+++ Book-Store/utils/bookDatabase.py
@@ -1,76 +1,98 @@
-import os
-
-books_file = "books.txt"
+import sqlite3
 
 
+# creating a database if not exist
 def createBook():
-    if os.path.isfile(books_file):
-        pass
-    else:
-        with open(books_file, 'w') as file:
-            pass
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS books (title text primary key, author text, year integer)")
+    connection.commit()
+    connection.close()
 
 
+# adding a book into the database
 def addBook(title, author, year):
-    with open(books_file, 'a') as file:
-        file.write(f'{title},{author},{year}\n')
-    print(f"Book titled {title} by {author} is added")
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO books VALUES(?, ?, ?)', (title, author, year))
+    connection.commit()
+    connection.close()
 
 
+# displaying all the books from the database
 def get_all_books():
-    with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
-
-    return [
-        {'title': line[0], 'author': line[1], 'year': line[2]}
-        for line in lines
-    ]
-
-
-def saveAllBooks(books):
-    with open(books_file, 'w') as file:
-        for book in books:
-            file.write(f"{book['title']},{book['author']},{book['year']}\n")
-
-
-def searchByTitle(title):
-    books = get_all_books()
-    books = [book for book in books if title in book['title']]
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM books')
+    books = [{'title': row[0],
+              'author': row[1],
+              'year': row[2]}
+             for row in cursor.fetchall()]
+    connection.close()
     return books
 
 
-def searchByAuthor(author):
-    books = get_all_books()
-    books = [book for book in books if author in book['author']]
-    return books
-
-
-def searchByYear(year):
-    books = get_all_books()
-    books = [book for book in books if year in book['year']]
-    return books
-
-
+# deleting a book from the database
 def deleteBook(title):
-    books = get_all_books()
-    books = [book for book in books if book['title'] != title]
-    saveAllBooks(books)
-    print(f"Book titled {title} is deleted")
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM books WHERE title =?', (title,))
+    connection.commit()
+    connection.close()
+    print(f"{title} is deleted....")
 
 
+# searching a book by title
+def searchBookByTitle(searchedTitle):
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM books WHERE title=?',(searchedTitle,))
+    books = [{'title': row[0],
+              'author': row[1],
+              'year': row[2]}
+             for row in cursor.fetchall()]
+    connection.close()
+    return books
+
+
+# searching a book by author
+def searchBookByAuthor(searchedAuthor):
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM books WHERE author=?',(searchedAuthor,))
+    books = [{'title': row[0],
+              'author': row[1],
+              'year': row[2]}
+             for row in cursor.fetchall()]
+    connection.close()
+    return books
+
+
+# searching a book by year
+def searchBookByYear(searchedYear):
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM books WHERE year=?',(searchedYear,))
+    books = [{'title': row[0],
+              'author': row[1],
+              'year': row[2]}
+             for row in cursor.fetchall()]
+    connection.close()
+    return books
+
+
+# updating a book into the database
 def updateBook(searchedBook, ch):
-    books = get_all_books()
-    for book in books:
-        if searchedBook == book['title']:
-            if ch == "T":
-                book['title'] = input("Enter the desired title : ")
-            elif ch == "A":
-                book['author'] = input("Enter the desired author : ")
-            elif ch == "Y":
-                book['year'] = input("Enter the desired year : ")
-            else:
-                print("Wrong choice")
-            print(f"Book(s) titled {searchedBook} is/are updated")
-        else:
-            pass
-    saveAllBooks(books)
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+    if ch == "T":
+        cursor.execute('UPDATE books SET title=? WHERE title=?',(input("Enter the desired title : "), searchedBook,))
+    elif ch == "A":
+        cursor.execute('UPDATE books SET author=? WHERE title=?',(input("Enter the desired author : "), searchedBook,))
+    elif ch == "Y":
+        cursor.execute('UPDATE books SET year=? WHERE title=?',(input("Enter the desired publishing year : "), searchedBook,))
+    else:
+        print("Wrong choice")
+    print(f"Book titled {searchedBook} is updated....")
+    connection.commit()
+    connection.close()
